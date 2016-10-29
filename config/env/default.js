ce56bb7da6137a89d4ae84207081cd80d8dd4dc2
Sunny Modi <Sunny Modi>
2016-10-29 14:49:42 +0530

    title change

--- config/env/default.js
+++ config/env/default.js
@@ -2,9 +2,8 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    title: 'API',
+    description: 'All data at one place',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   port: process.env.PORT || 3000,
