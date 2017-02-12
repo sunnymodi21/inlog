b3afce2048e1e096a5aa10b223890fb3b2159f61
Sunny Modi <Sunny Modi>
2017-02-13 00:42:24 +0530

    tiltle change

--- modules/core/client/directives/page-title.client.directive.js
+++ modules/core/client/directives/page-title.client.directive.js
@@ -18,7 +18,7 @@
       $rootScope.$on('$stateChangeSuccess', listener);
 
       function listener(event, toState) {
-        var applicationCoreTitle = 'API',
+        var applicationCoreTitle = 'Inlog- IOT for all',
           separeteBy = ' - ';
         if (toState.data && toState.data.pageTitle) {
           var stateTitle = $interpolate(toState.data.pageTitle)($state.$current.locals.globals);
